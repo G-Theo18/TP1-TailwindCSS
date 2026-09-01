396f205e3ea82556022af5115338e733c07afc88
tg853309 <tgou58@gmail.com>
2026-09-01 18:48:50 +0200

    Composant Bouton

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import Button from "./components/button";
+
 export function App() {
   return (
-	  <div>
-	  </div>
-	);
+    <div className="flex h-screen items-center justify-center gap-4">
+      <Button color="primary" size="md" shape="rounded">
+        Salut
+      </Button>
+
+      <Button color="secondary" size="lg" shape="pill">
+        Hello
+      </Button>
+
+      <Button color="tertiary" size="md" shape="rounded">
+        Halo
+      </Button>
+
+	    <Button>
+        Hola
+      </Button>
+    </div>
+  );
 }
---
+++ src/components/button.tsx
@@ -0,0 +1,46 @@
+import type { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+const button = tv({
+  base: "font-semibold tracking-wide disabled:opacity-50 cursor-pointer active:scale-105 hover:scale-110 transition-all duration-300",
+
+  variants: {
+    color: {
+      primary: "bg-primary text-white",
+      secondary: "bg-secondary text-white",
+      tertiary: "bg-tertiary text-white"
+    },
+
+    size: {
+      md: "py-3 px-6 rounded-xl",
+      lg: "py-4 px-8 rounded-2xl text-lg",
+    },
+
+    shape: {
+      pill: "rounded-full",
+      rounded: "",
+    },
+  },
+
+  defaultVariants: {
+    color: "primary",
+    size: "md",
+    shape: "rounded",
+  },
+});
+
+type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & VariantProps<typeof button>;
+
+export default function Button({ color, size, shape, className, ...rest }: ButtonProps) {
+  return (
+    <button
+      {...rest}
+      className={button({
+        color,
+        size,
+        shape,
+        className,
+      })}
+    />
+  );
+}
