6096b8e633bc24a8d2d5a7fa8e2b3a51add2f182
tg853309 <tgou58@gmail.com>
2026-09-02 17:04:15 +0200

    Préparation intégration

--- src/App.tsx
+++ src/App.tsx
@@ -9,68 +9,20 @@ import { ACTIVITIES } from "./data/activities.data";
 
 export function App() {
   return (
-    <>
-      <div className="flex h-screen flex-col items-center justify-center">
-        <div className="text-left">
-          <Heading text="primary" as="h2" size="display">
-            Heading Display
-          </Heading>
+    <div className="font-sans mx-auto">
+      <header>
+        {/* <Menu /> */}
+        {/* <Hero /> */}
+      </header>
 
-          <Heading text="primary" as="h2" size="lg">
-            Heading LG
-          </Heading>
+      <main className="container mx-auto px-6">
+        {/* <Benefits /> */}
+        {/* <Tours /> */}
+        {/* <Hotels /> */}
+        {/* <Blog /> */}
+      </main>
 
-          <Heading text="primary" as="h2" size="md">
-            Heading MD
-          </Heading>
-
-          <Heading text="primary" as="h2" size="sm">
-            Heading SM
-          </Heading>
-        </div>
-      </div>
-
-      <div className="flex h-screen items-center justify-center gap-4">
-        <Button color="primary" size="md" shape="rounded">
-          Salut
-        </Button>
-
-        <Button color="secondary" size="lg" shape="pill">
-          Hello
-        </Button>
-
-        <Button color="tertiary" size="md" shape="rounded">
-          Halo
-        </Button>
-
-        <Button color="whitegreen" size="md" shape="rounded">
-          Hola
-        </Button>
-      </div>
-
-      <div className="flex h-screen flex-col items-center justify-center gap-8">
-        {BENEFITS.map((benefit) => (
-          <Benefit
-            key={benefit.label}
-            benefit={benefit}
-          />
-        ))}
-      </div>
-
-      <div>
-        <Chips>
-          2 heures
-        </Chips>
-      </div>
-
-      <div className="flex justify-center gap-8">
-        {ACTIVITIES.map((activity) => (
-          <ActivityCard
-            key={activity.id}
-            activity={activity}
-          />
-        ))}
-      </div>
-    </>
+      {/* <Footer /> */}
+    </div>
   );
 }
