47f2234c09dcfb52820a43d2a5f1b4b69d444a2c
tg853309 <tgou58@gmail.com>
2026-09-09 08:40:43 +0200

    Responsive Menu

--- src/components/sections/menu.tsx
+++ src/components/sections/menu.tsx
@@ -1,12 +1,19 @@
 export default function Menu() {
   return (
-    <nav className="flex items-center justify-center my-10 gap-10">
-      <a href="#" className="hover:font-bold">Activités</a>
-      <a href="#" className="hover:font-bold">Expériences</a>
-      <a href="#" className="hover:font-bold">Açores</a>
-      <a href="#" className="hover:font-bold">Hôtels</a>
-      <a href="#" className="hover:font-bold">Spa</a>
+    <nav className="grid grid-cols-3 items-center my-10">
+      <div className="hidden md:flex col-start-1 items-center gap-10">
+        <a href="#" className="hover:font-bold">Activités</a>
+        <a href="#" className="hover:font-bold">Expériences</a>
+      </div>
+
+      <div className="col-start-2 flex justify-center">
+        <a href="#" className="font-bold text-lg">Açores</a>
+      </div>
+
+      <div className="hidden md:flex col-start-3 justify-end items-center gap-10">
+        <a href="#" className="hover:font-bold">Hôtels</a>
+        <a href="#" className="hover:font-bold">Spa</a>
+      </div>
     </nav>
   );
 }
-
